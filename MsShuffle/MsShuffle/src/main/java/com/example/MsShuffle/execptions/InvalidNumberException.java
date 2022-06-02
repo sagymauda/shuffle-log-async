fbc31f22dd54a2a257e262a4bd90588af246e3df
sagi <smauda85@gmail.com>
2022-06-02 15:41:14 +0300

    working!

--- MsShuffle/MsShuffle/src/main/java/com/example/MsShuffle/execptions/InvalidNumberException.java
+++
@@ -1,8 +0,0 @@
-package com.example.MsShuffle.execptions;
-
-public class InvalidNumberException extends Exception {
-
-    public InvalidNumberException(String msg) {
-        super(msg);
-    }
-}
--- MsShuffle/MsShuffle/src/main/java/com/example/MsShuffle/service/LogService.java
+++
@@ -1,7 +0,0 @@
-package com.example.MsShuffle.service;
-
-
-public interface LogService {
-
-    public void sendToLogService(int number);
-}
